6eae925ece0429cd1a4cb0f4b90cb7fb6c42aed1
Aliiel <leila.mouaci@gmail.com>
2024-09-03 17:00:38 +0200

    maj path controller forme juridique

--- src/main/java/fr/ln/nextLine/Controller/FormeJuridiqueController.java
+++ src/main/java/fr/ln/nextLine/Controller/FormeJuridiqueController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-nextline/formes-juridiques")
+@RequestMapping("/formes-juridiques")
 public class FormeJuridiqueController {
 
     private final FormeJuridiqueService formeJuridiqueService;
